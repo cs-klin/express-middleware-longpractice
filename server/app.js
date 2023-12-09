68bd838f0c8773a3e0cf2094ab52cdb4eb087267
Kai Lin <kevin_lin@alumni.brown.edu>
2023-12-09 15:14:41 -0500

    phase 2 done

--- server/app.js
+++ server/app.js
@@ -5,7 +5,18 @@ app.use(express.json());//add express.json middleware
 
 require('express-async-errors');// import express-async-errors' package
 
+app.use((req, res, next) => {
+  console.log(`${req.method}${req.url}`)
 
+
+
+
+res.on('finish', () => {
+  // read and log the status code of the response
+  console.log(`Status code: ${res.statusCode}` )
+})
+next()
+})
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -15,8 +26,9 @@ app.get('/', (req, res) => {
 app.post('/test-json', (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
+  console.log(req.body)
   res.json(req.body);
-  next();
+  // next();
 });
 
 // For testing express-async-errors
@@ -29,6 +41,21 @@ app.use((err,req,res,next) => {
   console.error(err.stack)
   res.status(500).send("Error: Hello World!")
 })
+
+
+
+
+const secondFunction = (req, res, next) => {
+  const error = new Error("The requested resource couldn't be found")
+  error.statusCode = 404
+  throw error
+}
+
+
+
+
+// app.use(logFunction)
+app.use(secondFunction)
 
 
 const port = 5000;
